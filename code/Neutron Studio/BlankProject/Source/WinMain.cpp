2c5513f8fc58d92ff26f7f8fbc26f1b970d8eb7a
BeginnerLevelDev <gtalover2@outlook.com>
2024-06-20 17:20:18 +1000

    .

---
+++ code/Neutron Studio/BlankProject/Source/WinMain.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, INT nCmdShow)
+{
+	/* - Create Window Class - */
+
+	/* - Create and Display our Window - */
+	
+	/* - Listen for Message events - */
+
+	return 0;
+
+}
